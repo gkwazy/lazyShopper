bd63c7d05ad7710de2a997ab06079f185e73cd68
ZCOLT <colton.zac@gmail.com>
2018-06-06 16:20:11 -0600

    walmart api

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("yo")
+
+
+$("#submit").on("click", function(event){
+    event.preventDefault();
+    
+    var buttonValue = $('#storeLocator').val()
+    console.log(buttonValue);
+    
+    var queryURL = "http://api.walmartlabs.com/v1/stores?format=json&zip=" + buttonValue + "&apiKey=dw49zcatfq86efbzy9yc2ku3"
+    
+    $.ajax({
+      url: queryURL,
+      headers: {
+        'X-Originating-IP': '65.130.232.218',
+        },
+      method: "GET"
+    }).done(function(storeLocator){
+      console.log(storeLocator);
+    })
+
+
+
+})
