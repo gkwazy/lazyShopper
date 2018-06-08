a87d811c6d729ef53f8f3628524421cfdcee6dc4
ZCOLT <colton.zac@gmail.com>
2018-06-07 20:28:44 -0600

    added walmart api to main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -29,3 +29,30 @@ $.ajax({
     }
 
 });
+
+$("#submit").on("click", function(event){
+    event.preventDefault();
+    
+    var buttonValue = $('#storeLocator').val()
+    console.log(buttonValue);
+    
+    var queryURL = "http://api.walmartlabs.com/v1/stores?format=json&zip=" + buttonValue + "&apiKey=dw49zcatfq86efbzy9yc2ku3"
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      dataType: "jsonp",
+    }).done(function(storeLocator){
+      console.log(storeLocator);
+      var closestStores = storeLocator.slice(0, 3);
+      console.log(closestStores);
+      for (var i = 0; i < closestStores.length; i++) {
+        //we need to gen a bunch of HTML elemts to put our store data inside
+        var div = $('<div></div>')
+        
+      }
+    })
+
+
+
+})
