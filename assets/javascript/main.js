5ea71ebfb72a78ecfe81fb597a9e6e11d4cb69bf
gkwazy <no-replay>
2018-06-09 12:30:39 -0600

    working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,82 +11,87 @@ console.log("yo");
 // firebase.initializeApp(config);
 // var database = firebase.database();
 
+var wantedItem;
+
+//ajaxCall();
+
+function ajaxCall() {
+    $(".listOfRecipes").empty();
+    $(".listOfNutrtion").empty();
+
+
+    var foodAppId = "ad90d902";
+    var foodAppKey = "92d0ccc447ac37767ca7d6859ff6a3ac";
+    //item that the user is searching for, var gathered 
+    //for, the search bar
+
+    //api key for the recipe api
+    var appId = "d6f00b57";
+    var appKey = "971c028b76c7d2aaa76db5c08e3acfbf";
+
+    var quaryURL = `https://api.edamam.com/search?q=${wantedItem}&app_id=${appId}&app_key=${appKey}&from=0&to=3&calories=591-722&health=alcohol-free`;
+    //var to help with all the information in the JOSN
+    var ingd;
+    //ajox call to the recipe api for the users information
+    $.ajax({
+
+        url: quaryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        ingd = response.hits;
+        console.log(response);
+        var label;
+        var listIngd = [];
+        //appending the wanted information to the HTML for the users view. 
+        for (i = 0; i < ingd.length; i++) {
+            $(".listOfNutrtion").append("<tr> ")
+
+            label = ingd[i].recipe.label;
+
+            for (j = 0; j < ingd[i].recipe.ingredientLines.length; j++) {
+                listIngd.push(ingd[i].recipe.ingredientLines[j]);
+                $(".listOfNutrtion").append("<td scope=col >" + ingd[i].recipe.ingredientLines[j] + "</td>")
 
 
 
-//api key for the nutrtion
-var foodAppId = "ad90d902";
-var foodAppKey = "92d0ccc447ac37767ca7d6859ff6a3ac";
-//item that the user is searching for, var gathered 
-//for, the search bar
-var wantedItem = "squash";
-//api key for the recipe api
-var appId = "d6f00b57";
-var appKey = "971c028b76c7d2aaa76db5c08e3acfbf";
-
-var quaryURL = `https://api.edamam.com/search?q=${wantedItem}&app_id=${appId}&app_key=${appKey}&from=0&to=3&calories=591-722&health=alcohol-free`;
-//var to help with all the information in the JOSN
-var ingd;
-//ajox call to the recipe api for the users information
-$.ajax({
-
-    url: quaryURL,
-    method: "GET"
-}).then(function (response) {
-
-    ingd = response.hits;
-    console.log(response);
-    var label;
-    var listIngd = [];
-    //appending the wanted information to the HTML for the users view. 
-    for (i = 0; i < ingd.length; i++) {
-        $(".listOfNutrtion").append("<tr> ")
-
-        label = ingd[i].recipe.label;
-
-        for (j = 0; j < ingd[i].recipe.ingredientLines.length; j++) {
-            listIngd.push(ingd[i].recipe.ingredientLines[j]);
-            $(".listOfNutrtion").append("<td scope=col >" + ingd[i].recipe.ingredientLines[j] + "</td>")
-
-
-
+            }
+            $(".listOfNutrtion").append("</tr>")
         }
-        $(".listOfNutrtion").append("</tr>")
-    }
 
-});
+    });
 
 
-//URL for the food nutrtion. 
-var quaryURL = `https://api.edamam.com/api/nutrition-data?app_id=${foodAppId}&app_key=${foodAppKey}&ingr=1%20large%20${wantedItem}`
-var foodIngd;
-//ajax call for the nutrition
-$.ajax({
+    //URL for the food nutrtion. 
+    var quaryURL = `https://api.edamam.com/api/nutrition-data?app_id=${foodAppId}&app_key=${foodAppKey}&ingr=1%20large%20${wantedItem}`
+    var foodIngd;
+    //ajax call for the nutrition
+    $.ajax({
 
-    url: quaryURL,
-    method: "GET"
-}).then(function (returned) {
+        url: quaryURL,
+        method: "GET"
+    }).then(function (returned) {
 
-    foodIngd = returned.hits;
-    console.log(returned)
-    var label;
-    var foodListIngd = [];
-    //appending the wanted information to the html for the user to view
+        foodIngd = returned.hits;
+        console.log(returned)
+        var label;
+        var foodListIngd = [];
+        //appending the wanted information to the html for the user to view
 
-    wantedInformation = ["FAT", "CHOCDF", "FIBTG"];
+        wantedInformation = ["FAT", "CHOCDF", "FIBTG"];
 
-    $(".listOfRecipes").append("<p> calories: " + returned.calories + "</p>");
-    for (i = 0; i < wantedInformation.length; i++) {
-        var nutritionInfor = wantedInformation[i];
-        var label = returned.totalNutrients[nutritionInfor].label;
-        var quantity = returned.totalNutrients[nutritionInfor].quantity;
-        var unit = returned.totalNutrients[nutritionInfor].unit;
-        var quantityPer = returned.totalDaily[nutritionInfor].quantity;
-        var unitPer = returned.totalDaily[nutritionInfor].unit;
-        $(".listOfRecipes").append("<p> " + label + ": " + Math.round(quantity) + unit + " Daily Amount " + Math.round(quantityPer) + unitPer + "</p>");
-    }
-});
-
+        $(".listOfRecipes").append("<p> calories: " + returned.calories + "</p>");
+        for (i = 0; i < wantedInformation.length; i++) {
+            var nutritionInfor = wantedInformation[i];
+            var label = returned.totalNutrients[nutritionInfor].label;
+            var quantity = returned.totalNutrients[nutritionInfor].quantity;
+            var unit = returned.totalNutrients[nutritionInfor].unit;
+            var quantityPer = returned.totalDaily[nutritionInfor].quantity;
+            var unitPer = returned.totalDaily[nutritionInfor].unit;
+            $(".listOfRecipes").append("<p> " + label + ": " + Math.round(quantity) + unit + " Daily Amount " + Math.round(quantityPer) + unitPer + "</p>");
+        }
+    });
+}
 
 $("#submit").on("click", function (event) {
     event.preventDefault();
@@ -135,8 +140,8 @@ $("#submit").on("click", function (event) {
 })
 A = [];
 
-var ingredients = ["Tomato", "Olive Oil", "Ground Beef", "Garlic"];
-
+var ingredients = [];
+renderButtons();
 // Function for displaying ingredient buttons
 function renderButtons() {
 
@@ -170,9 +175,24 @@ $("#add-item").on("click", function (event) {
     // This line will grab the text from the input box
     var ingredient = $("#item-input").val().trim();
     // The giphy from the textbox is then added to our array
-    ingredients.push(ingredient);
-
-    // calling renderButtons which handles the processing of our giphy array
+    console.log(ingredients.indexOf(ingredient));
+    console.log()
+    if (ingredients.indexOf(ingredient) < 0 && ingredient != "") {
+        ingredients.push(ingredient.toLowerCase());
+    }
+    // calling renderButtons which handles the processing of our array
     renderButtons();
+    $("#item-input").val("");
 });
 
+$("#new-item").on("click", ".ingredients", function () {
+    wantedItem = $(this).attr("data-name");
+    console.log(wantedItem);
+    //ajaxCall();
+
+});
+
+$("#clear-button").on("click", function (event) {
+    $("#new-item").empty();
+    ingredients = [];
+});
