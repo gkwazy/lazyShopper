db822b6c06dd8e76decedef0efa6ba7863294aac
gkwazy <noreply>
2018-06-09 01:38:44 -0600

    API working, need to get info on page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -15,12 +15,19 @@ console.log("yo");
 
 
 
-
+//api key for the nutrtion
+var foodAppId = "ad90d902";
+var foodAppKey = "92d0ccc447ac37767ca7d6859ff6a3ac";
+//item that the user is searching for, var gathered 
+//for, the search bar
+var wantedItem = "squash";
+//api key for the recipe api
 var appId = "d6f00b57";
 var appKey = "971c028b76c7d2aaa76db5c08e3acfbf";
-var wantedItem = "squash";
-var quaryURL = `https://api.edamam.com/search?q=${wantedItem}&app_id=${appId}&app_key=${appKey}&from=0&to=3&calories=591-722&health=alcohol-free`
+var quaryURL = `https://api.edamam.com/search?q=${wantedItem}&app_id=${appId}&app_key=${appKey}&from=0&to=3&calories=591-722&health=alcohol-free`;
+//var to help with all the information in the JOSN
 var ingd;
+//ajox call to the recipe api for the users information
 $.ajax({
 
     url: quaryURL,
@@ -28,66 +35,97 @@ $.ajax({
 }).then(function (response) {
 
     ingd = response.hits;
-
+    console.log(response);
     var label;
     var listIngd = [];
+    //appending the wanted information to the HTML for the users view. 
     for (i = 0; i < ingd.length; i++) {
-        $("#listOfItem").append("<tr> ")
+        $(".listOfNutrtion").append("<tr> ")
 
         label = ingd[i].recipe.label;
 
         for (j = 0; j < ingd[i].recipe.ingredientLines.length; j++) {
             listIngd.push(ingd[i].recipe.ingredientLines[j]);
-            $("#listOfItem").append("<td scope=col >" + ingd[i].recipe.ingredientLines[j] + "</td>")
+            $(".listOfNutrtion").append("<td scope=col >" + ingd[i].recipe.ingredientLines[j] + "</td>")
 
 
 
         }
-        $("#listOfItem").append("</tr>")
+        $(".listOfNutrtion").append("</tr>")
     }
 
 });
 
-$("#submit").on("click", function(event){
+
+//URL for the food nutrtion. 
+var quaryURL = `https://api.edamam.com/api/nutrition-data?app_id=${foodAppId}&app_key=${foodAppKey}&ingr=1%20large%20${wantedItem}`
+var foodIngd;
+//ajax call for the nutrition
+$.ajax({
+
+    url: quaryURL,
+    method: "GET"
+}).then(function (returned) {
+
+    foodIngd = returned.hits;
+    console.log(returned)
+    var label;
+    var foodListIngd = [];
+    //appending the wanted information to the html for the user to view
+
+    wantedInformation = ["CA", "FAT", "K"];
+    label = "retruned.totalDaily." + nutritionInfor + ".label";
+    quantity = "retruned.totalDaily." + nutritionInfor + ".quantity";
+    unit = "retruned.totalDaily." + nutritionInfor + ".unit";
+
+
+    $(".listOfRecipes").append("<p> calories: " + returned.calories + "</p>");
+    for (i = 0; i < wantedInformation.length; i++) {
+        var nutritionInfor = wantedInformation[i];
+        $(".listOfRecipes").append("<p> " + label + ":" + quantity + unit + "</p>");
+    }
+});
+
+$("#submit").on("click", function (event) {
     event.preventDefault();
     $('#closestStores').empty()
-    
+
     var buttonValue = $('#storeLocator').val()
     console.log(buttonValue);
-    
-    var queryURL = "http://api.walmartlabs.com/v1/stores?format=json&zip=" + buttonValue + "&apiKey=dw49zcatfq86efbzy9yc2ku3"
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-      dataType: "jsonp",
-    }).done(function(storeLocator){
-      console.log(storeLocator);
-      var closestStores = storeLocator.slice(0, 3);
-      console.log(closestStores);
-      for (var i = 0; i < closestStores.length; i++) {
-        //we need to gen a bunch of HTML elemts to put our store data inside
-        // var div = $('<div></div>')
-        
-        var name = storeLocator[i].name
-        var phoneNumber = storeLocator[i].phoneNumber
-        var streetAddress = storeLocator[i].streetAddress
-        var city = storeLocator[i].city 
-        var stateProvCode = storeLocator[i].stateProvCode
-        var zip = storeLocator[i].zip
-        
-        // div.attr("id", "closeStoreList")
-        // div.attr("data-store", "<strong>Store Name: </strong>" + name + "<br>" + "<strong>Phone Number: </strong>" + phoneNumber + "<br>" + "<strong>Address: </strong>" + streetAddress + " " + city + ", " + stateProvCode + " " + zip + "<br><br>")
 
-        $('#closestStores').append("<strong>Store Name: </strong>" + name + "<br>" + "<strong>Phone Number: </strong>" + phoneNumber + "<br>" + "<strong>Address: </strong>" + streetAddress + " " + city + ", " + stateProvCode + " " + zip + "<br><br>")
-        
-      }
-      console.log(storeLocator[0].name)
-      console.log(storeLocator[0].phoneNumber)
-      console.log(storeLocator[0].streetAddress)
-      console.log(storeLocator[0].city)
-      console.log(storeLocator[0].stateProvCode)
-      console.log(storeLocator[0].zip)
+    var queryURL = "http://api.walmartlabs.com/v1/stores?format=json&zip=" + buttonValue + "&apiKey=dw49zcatfq86efbzy9yc2ku3"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        dataType: "jsonp",
+    }).done(function (storeLocator) {
+        console.log(storeLocator);
+        var closestStores = storeLocator.slice(0, 3);
+        console.log(closestStores);
+        for (var i = 0; i < closestStores.length; i++) {
+            //we need to gen a bunch of HTML elemts to put our store data inside
+            // var div = $('<div></div>')
+
+            var name = storeLocator[i].name
+            var phoneNumber = storeLocator[i].phoneNumber
+            var streetAddress = storeLocator[i].streetAddress
+            var city = storeLocator[i].city
+            var stateProvCode = storeLocator[i].stateProvCode
+            var zip = storeLocator[i].zip
+
+            // div.attr("id", "closeStoreList")
+            // div.attr("data-store", "<strong>Store Name: </strong>" + name + "<br>" + "<strong>Phone Number: </strong>" + phoneNumber + "<br>" + "<strong>Address: </strong>" + streetAddress + " " + city + ", " + stateProvCode + " " + zip + "<br><br>")
+
+            $('#closestStores').append("<strong>Store Name: </strong>" + name + "<br>" + "<strong>Phone Number: </strong>" + phoneNumber + "<br>" + "<strong>Address: </strong>" + streetAddress + " " + city + ", " + stateProvCode + " " + zip + "<br><br>")
+
+        }
+        console.log(storeLocator[0].name)
+        console.log(storeLocator[0].phoneNumber)
+        console.log(storeLocator[0].streetAddress)
+        console.log(storeLocator[0].city)
+        console.log(storeLocator[0].stateProvCode)
+        console.log(storeLocator[0].zip)
 
 
     })
