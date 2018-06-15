837d769a72d81a32daa052322c7af590d7be9f9b
ZCOLT <colton.zac@gmail.com>
2018-06-14 18:38:59 -0600

    take 2

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,5 @@
 console.log("yo");
 
-
-
 var config = {
     apiKey: "AIzaSyDWjwGAMxs6Xcd-wGfz-Fgi960RZLhJ70s",
     authDomain: "lazyshopper-3cd30.firebaseapp.com",
@@ -91,31 +89,46 @@ database.ref('users/').on('value', function(snapshot){
     console.log(snapshot.val());
 })
 
-//add saved items to page from firebase
-// database.ref().on("child_added", function (snapshot) {
- 
-//   var name = snapshot.val().name;
-//   console.log(name);
-// })
-
 
 var wantedItem;
-var wantedURL = [];
 
-$('#add-item').on("click", function (event) {
+$('#add-item').on("click", function (event){
     event.preventDefault();
 
-    // database.ref().push(savedItem);
-  
+    //set wantedItem to input from user
+    wantedItem = $('#item-input').val().trim()
+    // console.log(wantedItem)
+
+    //send wanted item to firebase 
+    var savedItem = {
+        name: wantedItem,
+    }
+    console.log(savedItem)
+
+    database.ref().push(savedItem);
+
+    console.log(savedItem.name);
+
+    //   alert("Item successfully added");
+
+    //   $("#item-input").val("");
+
     ajaxCall();
+    
+})
+//add saved items to page from firebase
+database.ref().on("child_added", function (snapshot) {
+ 
+  var name = snapshot.val().name;
+  console.log(name);
 })
 
 
+//ajaxCall();
 
+// ajaxCall();
 
-
-loadList();
-//$('#myModal').modal({ show: true });
+$('#myModal').modal({ show: true });
 
 function ajaxCall() {
     $(".listOfRecipes").empty();
@@ -126,6 +139,8 @@ function ajaxCall() {
     var foodAppKey = "92d0ccc447ac37767ca7d6859ff6a3ac";
     //item that the user is searching for, var gathered 
     //for, the search bar
+
+
 
     //api key for the recipe api
     var appId = "d6f00b57";
@@ -150,8 +165,8 @@ function ajaxCall() {
             $(".listOfNutrtion").append("<br> ");
 
             label = ingd[i].recipe.label;
-            console.log("url " + ingd[i].recipe.url);
-            $(".listOfNutrtion").append("<h1 class='label-click' data-recipe = '" + ingd[i].recipe.url + " data-name = '" + label + "'>" + label + "<h1>");
+
+            $(".listOfNutrtion").append("<h1>" + label + "<h1>");
 
             for (j = 0; j < ingd[i].recipe.ingredientLines.length; j++) {
                 listIngd.push(ingd[i].recipe.ingredientLines[j]);
@@ -159,6 +174,7 @@ function ajaxCall() {
 
 
             }
+
 
         }
 
@@ -199,9 +215,9 @@ function ajaxCall() {
 
 $("#submit").on("click", function (event) {
     event.preventDefault();
-    $('#closestStores').empty();
+    $('#closestStores').empty()
 
-    var buttonValue = $('#storeLocator').val();
+    var buttonValue = $('#storeLocator').val()
     console.log(buttonValue);
 
     var queryURLWalmart = `http://api.walmartlabs.com/v1/stores?format=json&zip=${buttonValue}&apiKey=dw49zcatfq86efbzy9yc2ku3`
@@ -216,6 +232,7 @@ $("#submit").on("click", function (event) {
         console.log(closestStores);
         for (var i = 0; i < closestStores.length; i++) {
             //we need to gen a bunch of HTML elemts to put our store data inside
+            // var div = $('<div></div>')
 
             var name = storeLocator[i].name
             var phoneNumber = storeLocator[i].phoneNumber
@@ -223,6 +240,9 @@ $("#submit").on("click", function (event) {
             var city = storeLocator[i].city
             var stateProvCode = storeLocator[i].stateProvCode
             var zip = storeLocator[i].zip
+
+            // div.attr("id", "closeStoreList")
+            // div.attr("data-store", "<strong>Store Name: </strong>" + name + "<br>" + "<strong>Phone Number: </strong>" + phoneNumber + "<br>" + "<strong>Address: </strong>" + streetAddress + " " + city + ", " + stateProvCode + " " + zip + "<br><br>")
 
             $('#closestStores').append(`<strong>Store Name: </strong>${name}<br><strong>Phone Number: </strong>${phoneNumber}<br><strong>Address: </strong>${streetAddress} ${city}, ${stateProvCode} ${zip}<br><br>`)
 
@@ -303,55 +323,3 @@ $("#clear-button").on("click", function (event) {
 
     ingredients = [];
 });
-$("#save").on("click", function (event) {
-    var email = $('#user-name').val();
-    var password = $('#password-text').val();
-    console.log(email + password);
-
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here.
-        console.log(error)
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
-})
-
-$("#dump-item").on("click", ".label-click", function (event) {
-    event.preventDefault();
-    wantedURL = [];
-    console.log("button clicked")
-    var newURL = $(this).attr("data-recipe");
-    database.ref("URLs").once("value", function (snapshot) {
-        for (i = 0; i < snapshot.val().wantedURL.length; i++) {
-            wantedURL.push(snapshot.val().wantedURL[i]);
-            console.log((snapshot.val()));
-        }
-
-        console.log(wantedURL + ":wantedURL")
-        console.log(newURL + " this stuff")
-        if (wantedURL.indexOf(newURL) < 0) {
-            wantedURL.push(newURL);
-            console.log(wantedURL);
-        }
-        database.ref("URLs").set({
-            wantedURL
-        });
-    });
-    $(".recipeAdd").empty();
-    for (i = 0; i < wantedURL.length; i++) {
-        $(".recipeAdd").append("<br>" + wantedURL[i] + "</br>")
-    }
-});
-
-function loadList() {
-    console.log("loadList ran");
-    $(".recipeAdd").empty();
-    database.ref("URLs").once("value", function (snapshot) {
-        for (i = 0; i < snapshot.val().wantedURL.length; i++) {
-            wantedURL.push(snapshot.val().wantedURL[i]);
-            $(".recipeAdd").append("<br>" + wantedURL[i].link(wantedURL[i]) + "</br>")
-            console.log("load list wanted; " + wantedURL[i])
-        }
-    });
-};
